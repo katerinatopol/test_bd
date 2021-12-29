be3546e16cfcb6f15f6e83e791cd47a824229960
katerina_topol <katerina_topol@mail.ru>
2021-12-29 23:13:24 +0300

    add classes

---
+++ classes.py
@@ -0,0 +1,35 @@
+class Ship:
+
+    def __init__(self, ship, weapon, hull, engine):
+        self.ship = ship
+        self.weapon = weapon
+        self.hull = hull
+        self.engine = engine
+
+
+class Weapon:
+
+    def __init__(self, weapon, reload_speed, rotation_speed, diameter, power_volley, count):
+        self.weapon = weapon
+        self.reload_speed = reload_speed
+        self.rotation_speed = rotation_speed
+        self.diameter = diameter
+        self.power_volley = power_volley
+        self.count = count
+
+
+class Hull:
+
+    def __init__(self, hull, armor, type, capacity):
+        self.hull = hull
+        self.armor = armor
+        self.type = type
+        self.capacity = capacity
+
+
+class Engine:
+
+    def __init__(self, engine, power, type):
+        self.engine = engine
+        self.power = power
+        self.type = type
--- create_bd.py
+++ create_bd.py
@@ -2,90 +2,109 @@ import sqlite3
 
 from random import randint
 
-try:
-    # Создаем базу данных и объект курсор
-    connection = sqlite3.connect('test_bd.db')
-    cursor = connection.cursor()
-
-    # TODO связи ключей
-
-    # Создаем таблицу Ships
-    cursor.execute("""CREATE TABLE IF NOT EXISTS Ships(
-       ship TEXT PRIMARY KEY,
-       weapon TEXT,
-       hull TEXT,
-       engine TEXT);
-    """)
-    connection.commit()
-
-    # Создаем таблицу Weapons
-    cursor.execute("""CREATE TABLE IF NOT EXISTS Weapons(
-       weapon TEXT PRIMARY KEY,
-       reload_speed INT,
-       rotation_speed INT,
-       diameter INT,
-       power_volley INT,
-       count INT);
-    """)
-    connection.commit()
-
-    # Создаем таблицу Hulls
-    cursor.execute("""CREATE TABLE IF NOT EXISTS Hulls(
-       hull TEXT PRIMARY KEY,
-       armor INT,
-       type INT,
-       capacity INT);
-    """)
-    connection.commit()
-
-    # Создаем таблицу Engines
-    cursor.execute("""CREATE TABLE IF NOT EXISTS Engines(
-       engine TEXT PRIMARY KEY,
-       power INT,
-       type INT);
-    """)
-    connection.commit()
+from test_data import SHIPS_COUNT, WEAPONS_COUNT, HULLS_COUNT, ENGINES_COUNT
 
 
-    # Создаем рандомные данные
-    ships = [(f'Ship-{i+1}', f'Weapon-{i+1}', f'Hull-{i+1}', f'Engine-{i+1}') for i in range(200)]
-    weapons = [(f'Weapon-{i+1}', randint(1, 20), randint(1, 20), randint(1, 20), randint(1, 20), randint(1, 20))
-               for i in range(20)]
-    hulls = [(f'Hull-{i+1}', randint(1, 20), randint(1, 20), randint(1, 20)) for i in range(5)]
-    engines = [(f'Engine-{i+1}', randint(1, 20), randint(1, 20)) for i in range(6)]
+class DataBase:
+
+    def __init__(self, name):
+        self.name = name
+        try:
+            self.connection = sqlite3.connect(f'{name}.db')
+            self.cursor = self.connection.cursor()
+
+        except sqlite3.Error as error:
+            print(f"Ошибка при подключении к sqlite {error}")
+
+    def create_tables(self):
+        try:
+            # Создаем таблицу Ships
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS Ships(
+               ship TEXT PRIMARY KEY,
+               weapon TEXT,
+               hull TEXT,
+               engine TEXT,
+               FOREIGN KEY (weapon) REFERENCES Weapons(weapon),
+               FOREIGN KEY (hull) REFERENCES Hulls(hull),
+               FOREIGN KEY (engine) REFERENCES Engines(engine));
+            """)
+            self.connection.commit()
+
+            # Создаем таблицу Weapons
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS Weapons(
+               weapon TEXT PRIMARY KEY,
+               reload_speed INT,
+               rotation_speed INT,
+               diameter INT,
+               power_volley INT,
+               count INT);
+            """)
+            self.connection.commit()
+
+            # Создаем таблицу Hulls
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS Hulls(
+               hull TEXT PRIMARY KEY,
+               armor INT,
+               type INT,
+               capacity INT);
+            """)
+            self.connection.commit()
+
+            # Создаем таблицу Engines
+            self.cursor.execute("""CREATE TABLE IF NOT EXISTS Engines(
+               engine TEXT PRIMARY KEY,
+               power INT,
+               type INT);
+            """)
+            self.connection.commit()
+
+        except sqlite3.Error as error:
+            print(f"Ошибка при подключении к sqlite {error}")
+
+    def insert_test_data(self):
+
+        # Создаем рандомные данные
+        ships = [(f'Ship-{i + 1}', f'Weapon-{randint(1, 20)}', f'Hull-{randint(1, 5)}', f'Engine-{randint(1, 6)}')
+                 for i in range(SHIPS_COUNT)]
+        weapons = [(f'Weapon-{i + 1}', randint(1, 20), randint(1, 20), randint(1, 20), randint(1, 20), randint(1, 20))
+                   for i in range(WEAPONS_COUNT)]
+        hulls = [(f'Hull-{i + 1}', randint(1, 20), randint(1, 20), randint(1, 20)) for i in range(HULLS_COUNT)]
+        engines = [(f'Engine-{i + 1}', randint(1, 20), randint(1, 20)) for i in range(ENGINES_COUNT)]
+
+        # Заполняем таблицы созданными данными
+
+        self.cursor.execute("DELETE FROM ships")
+        self.connection.commit()
+        self.cursor.executemany("INSERT INTO ships VALUES(?, ?, ?, ?);", ships)
+        self.connection.commit()
+
+        self.cursor.execute("DELETE FROM weapons")
+        self.connection.commit()
+        self.cursor.executemany("INSERT INTO weapons VALUES(?, ?, ?, ?, ?, ?);", weapons)
+        self.connection.commit()
+
+        self.cursor.execute("DELETE FROM hulls")
+        self.connection.commit()
+        self.cursor.executemany("INSERT INTO hulls VALUES(?, ?, ?, ?);", hulls)
+        self.connection.commit()
+
+        self.cursor.execute("DELETE FROM engines")
+        self.connection.commit()
+        self.cursor.executemany("INSERT INTO engines VALUES(?, ?, ?);", engines)
+        self.connection.commit()
+
+    def close_connection(self):
+        if self.connection:
+            self.cursor.close()
+            self.connection.close()
 
 
-    # Заполняем таблицы созданными данными
-    # TODO продумать объекты/модели
+def main():
+    test_db = DataBase('wargaming')
+    test_db.create_tables()
+    test_db.insert_test_data()
+    test_db.close_connection()
 
-    cursor.execute("DELETE FROM ships")
-    connection.commit()
 
-    cursor.executemany("INSERT INTO ships VALUES(?, ?, ?, ?);", ships)
-    connection.commit()
-
-    cursor.execute("DELETE FROM weapons")
-    connection.commit()
-
-    cursor.executemany("INSERT INTO weapons VALUES(?, ?, ?, ?, ?, ?);", weapons)
-    connection.commit()
-
-    cursor.execute("DELETE FROM hulls")
-    connection.commit()
-
-    cursor.executemany("INSERT INTO hulls VALUES(?, ?, ?, ?);", hulls)
-    connection.commit()
-
-    cursor.execute("DELETE FROM engines")
-    connection.commit()
-
-    cursor.executemany("INSERT INTO engines VALUES(?, ?, ?);", engines)
-    connection.commit()
-
-except sqlite3.Error as error:
-    print(f"Ошибка при подключении к sqlite {error}")
-
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
+if __name__ == '__main__':
+    main()
