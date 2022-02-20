1a376b16a4592d5bee43d09e116c19ae04aafcaa
katerina_topol <katerina_topol@mail.ru>
2022-02-20 17:51:48 +0300

    fix logger

--- models/database_model.py
+++ models/database_model.py
@@ -14,10 +14,10 @@ class DataBase:
         try:
             self.connection = sqlite3.connect(f'{name}.db')
             self.cursor = self.connection.cursor()
-            Logger.info("Open connection with database %s" % name)
+            Logger.info(f"Open connection with database {name}")
 
         except sqlite3.Error as error:
-            Logger.error("Error connecting to sqlite %s" % error)
+            Logger.error(f"Error connecting to sqlite {error}")
 
     def create_tables(self):
         for table in TABLES.values():
@@ -32,35 +32,35 @@ class DataBase:
             try:
                 self.cursor.execute(f"""CREATE TABLE IF NOT EXISTS {table['name_table']}({columns});""")
                 self.connection.commit()
-                Logger.info("Create table %s" % table['name_table'])
+                Logger.info(f"Create table {table['name_table']}")
 
             except sqlite3.Error as error:
-                Logger.error("Error connecting to sqlite %s" % error)
+                Logger.error(f"Error connecting to sqlite {error}")
 
     def insert_test_data(self, name_table, data):
         self.cursor.execute(f"DELETE FROM {name_table}")
         self.cursor.executemany(f"INSERT INTO {name_table} VALUES({', '.join(['?' for _ in range(len(data[0]))])});",
                                 data)
         self.connection.commit()
-        Logger.info("Table %s filled with data" % name_table)
+        Logger.info(f"Table {name_table} filled with data")
 
     def close_connection(self):
         if self.connection:
             self.cursor.close()
             self.connection.close()
-            Logger.info("Closed connection to database %s" % self.name)
+            Logger.info(f"Closed connection to database {self.name}")
 
     def update(self, name_table, change_elem, new_val, key, condition):
         self.cursor.execute(f"UPDATE {name_table} SET {change_elem} = '{new_val}' WHERE {key} = ? ;", (condition, ))
         self.connection.commit()
-        Logger.info("Updated data in the table %s" % name_table)
+        Logger.info(f"Updated data in the table {name_table}")
 
     def select(self, name_table, key=None, condition=None, column='*', all_info=False):
         if key and condition:
             value = self.cursor.execute(f"""SELECT {column} FROM {name_table} WHERE {key} = '{condition}';""").fetchall()
         else:
             value = self.cursor.execute(f"""SELECT {column} FROM {name_table};""").fetchall()
-        Logger.info("Table %s search completed" % name_table)
+        Logger.info(f"Table {name_table} search completed")
         if all_info:
             return value
         else:
@@ -68,7 +68,7 @@ class DataBase:
 
     def copy_database(self, name_new_db):
         self.cursor.execute(f"""vacuum into '{name_new_db}.db';""")
-        Logger.info("Create copy database %s" % self.name)
+        Logger.info(f"Create copy database {self.name}")
 
         return DataBase(name_new_db)
 
